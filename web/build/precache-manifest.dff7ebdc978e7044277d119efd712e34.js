c2f0a53fe4d9d55fbc8b0c6a46d182a59069d788
Nitesh <jhanitesh10@gmail.com>
2020-09-05 22:46:01 +0530

    Move to web directory

---
+++ web/build/precache-manifest.dff7ebdc978e7044277d119efd712e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4d5e85ee0f23bca73f8e1433cebc47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "596da7430e5494dcb947",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "98468590c38a535d24bf",
+    "url": "/static/js/2.0f15a648.chunk.js"
+  },
+  {
+    "revision": "bb0d2e924b7b6743d70dbdc0ece34a4f",
+    "url": "/static/js/2.0f15a648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "596da7430e5494dcb947",
+    "url": "/static/js/main.3e6ebc13.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  }
+]);
